7fc72cccf70399382022287c97b722b39a136020
Arrowmarkstudios <carlosgarciac0502@gmail.com>
2026-03-24 10:56:25 -0700

    Delete scripts.js

--- scripts.js
+++
@@ -1,5 +0,0 @@
-﻿// This can be used to handle form submissions or other dynamic events in the future.
-document.querySelector("form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Thank you for contacting JMR Auto Detailing! We'll get back to you soon.");
-});
